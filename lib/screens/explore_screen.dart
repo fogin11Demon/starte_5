dae49947dbb35f73fe555ba87cca3e87beb385a0
Hunter <dekteryo@yandex.ru>
2022-04-12 21:14:38 +0300

    gg

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -2,38 +2,40 @@ import 'package:flutter/material.dart';
 import '../components/components.dart';
 import '../models/models.dart';
 import '../api/mock_fooderlich_service.dart';
+
 class ExploreScreen extends StatelessWidget {
   // 1
   final mockService = MockFooderlichService();
   ExploreScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // 2
-    // TODO 1: Add TodayRecipeListView FutureBuilder
     // 1
     return FutureBuilder(
-        // 2
-        future: mockService.getExploreData(),
-    // 3
-    builder: (context, AsyncSnapshot<ExploreData> snapshot) {
-      // TODO: Add Nested List Views
-      // 4
-      if (snapshot.connectionState == ConnectionState.done) {
-        // 5
-        final recipes = snapshot.data?.todayRecipes ?? [];
-        return TodayRecipeListView(recipes: recipes);
-        return Center(
-          child: Container(
-            child: const Text('Show TodayRecipeListView'),
-          ),
-        );
-      } else {
-        // 6
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      }
-    },
+      // 2
+      future: mockService.getExploreData(),
+      // 3
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+        // 4
+        if (snapshot.connectionState == ConnectionState.done) {
+          // 5
+          return ListView(
+            // 6
+            scrollDirection: Axis.vertical,
+            children: [
+              // 7
+              TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
+              // 8
+              const SizedBox(height: 16),
+              // 9
+              FriendPostListView(friendPosts: snapshot.data?.friendPosts ??
+                  []),
+            ],
+          );
+        } else {
+          // 10
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
-    }
   }
+}
